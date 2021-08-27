d66242cd84e04d8a9d442de3cc3adbf1485c20c4
Daniel-Tshragyan <dchragyan1@gmail.com>
2021-08-27 14:30:18 +0300

    Created opportunitey to add pricing plan for comanies

--- resources/views/frontend/company/job/create.blade.php
+++ resources/views/frontend/company/job/create.blade.php
@@ -42,9 +42,7 @@
                                 <input type="text" value="{{ old('title') }}" class="form-control" name="title"
                                        placeholder="Write job title">
                             </div>
-                            <input type="text" name="a[]" value="a">
-                            <input type="text" name="a[]" value="ab">
-                            <input type="text" name="a[]" value="ac">
+
                             @if ($errors->has('title'))
                                 <div class="alert alert-danger" role="alert">
                                     {{ $errors->first('title') }}
